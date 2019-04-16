0fb0be75ae580c3846d1620d4c4830ca6d899343
diondree <diondre27@hotmail.com>
2019-04-15 23:29:18 -0400

    Edit to Adsense code

--- src/util/index.js
+++ src/util/index.js
@@ -49,13 +49,13 @@ function getTax(salary) {
 
   // if amount is 35000 or under then income tax is 16%
   if (taxable <= 35000) {
-    return (taxable * 16) / 100;
+    return (taxable * 12.5) / 100;
   }
 
   // if amount is over 35000 and less than or equal to 50 000
   // then the 35000 is done at 16% and the remainder is done at 33.5%
   if (taxable > 35000 && taxable <= 50000) {
-    const bracket1 = (35000 * 16) / 100;
+    const bracket1 = (35000 * 12.5) / 100;
     const remainder = ((taxable - 35000) * 33.5) / 100;
     return bracket1 + remainder;
   }
