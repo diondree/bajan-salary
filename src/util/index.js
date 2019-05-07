ce407071689d43d272b52857bbf2bd09e48860da
diondree <diondre27@hotmail.com>
2019-05-06 21:31:48 -0400

    Fix tax rate

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,7 @@
 const ALLOWANCE = 25000;
+
+// TODO Use insurable earnings instead
+// You are covered by the NIS up to the first $1,112 per week or $4,820 per month.
 const NIS_CEILINGS = {
   bimonthly: '267.51',
   monthly: '535.02',
@@ -49,13 +52,13 @@ function getTax(salary) {
 
   // if amount is 35000 or under then income tax is 16%
   if (taxable <= 35000) {
-    return (taxable * 12.5) / 100;
+    return (taxable * 16) / 100;
   }
 
   // if amount is over 35000 and less than or equal to 50 000
   // then the 35000 is done at 16% and the remainder is done at 33.5%
   if (taxable > 35000 && taxable <= 50000) {
-    const bracket1 = (35000 * 12.5) / 100;
+    const bracket1 = (35000 * 16) / 100;
     const remainder = ((taxable - 35000) * 33.5) / 100;
     return bracket1 + remainder;
   }
